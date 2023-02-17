80c6f657b12e4ff952104877b8a2c97b87ed7e0f
Pratyendu Sarkar <pratyendu@gmail.com>
2023-02-18 02:05:39 +0530

    umsdb

--- instagram/src/main/java/com/geekster/instagram/Model/User.java
+++ instagram/src/main/java/com/geekster/instagram/Model/User.java
@@ -1,4 +1,23 @@
 package com.geekster.instagram.Model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tbl_user")
+
 public class User {
+    @Id //primary key
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int userId;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "age")
+    private int age;
+
 }
