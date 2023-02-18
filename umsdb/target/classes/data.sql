5d51b202589a4ee7ce9ff5a2ccae336d7a944100
Pratyendu <101126984+Pratyendu@users.noreply.github.com>
2023-02-18 20:29:34 +0530

    User management System with H2 DB and validation

---
+++ umsdb/target/classes/data.sql
@@ -0,0 +1,4 @@
+insert into "user" ("date_of_birth" , "date_time" , "email" , "phone_number" , "user_name") values ('24-06-1998' , '13/2/23 17:51' , 'pratyendu@gmail.com' , '6291333357' , 'Pratyendu');
+insert into "user" ("date_of_birth" , "date_time" , "email" , "phone_number" , "user_name") values ('02-06-1996' , '13/2/23 18:22' , 'abc@gmail.com' , '6123434357' , 'abc');
+
+
